af2cb35207590f1927dca0a7a5df7e6e5ce559cf
Johnzel Tuddao <42705324+J0hnZMT@users.noreply.github.com>
2019-10-31 11:05:39 +0800

    Update scraper.py

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -9,6 +9,7 @@ from requests.exceptions import RequestException
 import contextlib
 import csv
 from datetime import datetime
+csv_name = ['']
 
 
 def is_good_response(response):
@@ -19,7 +20,7 @@ def is_good_response(response):
             and content_type.find('html') > -1)
 
 
-def get_url(url_to_access):
+def get_content(url_to_access):
     # open the content of the url
     try:
         url_to_open = requests.get(url_to_access, stream=True)
@@ -31,12 +32,6 @@ def get_url(url_to_access):
 
     except RequestException as e:
         print('Error during requests to {} : {}'.format(url_to_access, str(e)))
-
-
-def get_current_url(accessed_url):
-    # return thr current url accessed
-    current = requests.get(accessed_url)
-    return current.url
 
 
 def get_link(resp):
@@ -57,19 +52,18 @@ def get_the_links(connected_url, links):
             new_url = '{}{}'.format(connected_url, link)
             go_to_url(new_url)
         else:
-            return receive_link
+            return receive_links
 
 
 def go_to_url(url_to_connect):
-    current_url = get_current_url(url_to_connect)
-    response = get_url(url_to_connect)
+    response = get_content(url_to_connect)
     return_links = get_link(response)
-    csv_write(current_url, return_links)
+    csv_write(url_to_connect, return_links)
 
 
 def get_size(url_to_go):
     # getting the size of the file
-    html_content = get_url(url_to_go)
+    html_content = get_content(url_to_go)
     html = BeautifulSoup(html_content, 'html.parser')
     data = []
     for td in html.find_all('td'):
@@ -86,14 +80,13 @@ def csv_date_time():
 
 
 def csv_write(url, links):
-    csv_file_name = csv_date_time()
+    global csv_name
+    csv_file_name = csv_name[0]
     file_names = get_the_links(url, links) # return the list of filenames
     file_size = get_size(url) # return the list of sizes
-    with open(csv_file_name, "a+", newline='', encoding='utf-8') as csv_file:
+    with open(csv_file_name, "a", newline='', encoding='utf-8') as csv_file:
         # content of the csv file
-        if file_names is None:
-            print("Complete! {} saved.".format(csv_file_name))
-        else:
+        if file_names is not None:
             for filename, size in zip(file_names, file_size):
                 output = [filename, url+filename, size]
                 csv_writer = csv.writer(csv_file)
@@ -101,18 +94,28 @@ def csv_write(url, links):
                 print(output)
 
 
-def main():
-    base_url = 'http://mirror.rise.ph/centos/7/'
-    response = get_url(base_url) # get the content of the url
-    return_links = get_link(response) # get the links found
-    get_the_links(base_url, return_links)
+def csv_header():
+    global csv_name
     csv_file_name = csv_date_time()
     with open(csv_file_name, "w+", newline='', encoding='utf-8') as csv_file:
         # header for the csv file
         field_header = ['Filename', 'Download link', 'Filesize']
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(field_header)
+    csv_name[0] = csv_file_name
+
+
+def main(base_url):
+    global csv_name
+    response = get_content(base_url) # get the content of the url
+    return_links = get_link(response) # get the links found
+    received_links = get_the_links(base_url, return_links)
+    if received_links is not None:
+        main()
+    elif received_links is None:
+        print("Complete {} saved.".format(csv_name[0]))
 
 
 if __name__ == '__main__':
-    main()
+    csv_header()
+    main('http://mirror.rise.ph/centos/7/')
